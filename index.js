1961ea3dd7058f4bd3c95f5f7f97c4d7a4bff12a
fb@enyosolutions.com <fb@enyosolutions.com>
2023-01-04 18:16:34 +0000

    fix: my ip

--- index.js
+++ index.js
@@ -7,14 +7,16 @@ const app = express();
 
 app.get(['/', '/ip'], async (req, res) => {
   const result = {};
+  const ip =
+    req.headers['x-real-ip'] || req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   try {
-    const { data } = await axios.get(`https://ipapi.co/${req.ip}/json/`);
+    const { data } = await axios.get(`https://ipapi.co/${ip}/json/`);
     result.data = data;
   } catch (err) {
     console.warn(err);
   }
   res.json({
-    ip: req.ip,
+    ip,
     ...result,
   });
 });
