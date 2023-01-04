af24f3b3cbebdafeb8d0700d408a913b372040f7
fb@enyosolutions.com <fb@enyosolutions.com>
2023-01-04 19:05:52 +0000

    feat: local addresses

--- cli.js
+++ cli.js
@@ -2,10 +2,24 @@
 // create javascript app to return the ip address of the client
 
 const axios = require('axios');
+// get local ip from os
+const os = require('os');
+
+const interfaces = os.networkInterfaces();
+const addresses = []; // eslint-disable-line
+for (const k in interfaces) {
+  for (const k2 in interfaces[k]) {
+    const address = interfaces[k][k2];
+    if (address.family === 'IPv4' && !address.internal) {
+      addresses.push(address.address);
+    }
+  }
+}
 
 axios
   .get(process.env.MYIP_SERVER || 'https://ip.enyo.cc/ip')
   .then(({ data }) => {
+    data.local = addresses;
     console.log(data);
   })
   .catch((err) =>
